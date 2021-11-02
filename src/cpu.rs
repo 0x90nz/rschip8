95733645544bd7b34d31c945c33f1a8d13194935
Paul Bouda <paul@nop.nz>
2021-11-02 13:13:38 +1300

    Add all instructions but sound, disp, input and BCD

--- src/cpu.rs
+++ src/cpu.rs
@@ -2,6 +2,7 @@ use std::cmp;
 use std::convert::TryInto;
 use std::iter::Iterator;
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct Registers {
     v_regs: [u8; 16],
     i: u16,
@@ -9,6 +10,7 @@ pub struct Registers {
     sp: u16, // this can be 8 bit?
 }
 
+#[derive(Debug)]
 pub struct CPU {
     regs: Registers, 
     memory: Vec<u8>,
@@ -16,6 +18,7 @@ pub struct CPU {
     dt: u8, // delay timer
     st: u8, // sound timer,
     timedelta_error: u32, // the number of ms not used in the previous cycle(s)
+    prng_val: u32,
 }
 
 fn bytes_to_nibbles<'a>(bytes: impl Iterator<Item = &'a u8>) -> Vec<u8> {
@@ -25,6 +28,29 @@ fn bytes_to_nibbles<'a>(bytes: impl Iterator<Item = &'a u8>) -> Vec<u8> {
         v.push(b & 0x0f);
     }
     v
+}
+
+pub fn nibbles_to_bytes<'a>(nibbles: impl Iterator<Item = &'a u8>) -> Vec<u8> {
+    let mut v: Vec<u8> = Vec::new();
+    let mut first_half = true;
+
+    for b in nibbles {
+        if first_half {
+            v.push(*b);
+        } else {
+            let idx = v.len() - 1;
+            v[idx] = v[idx] << 4 | *b;
+        }
+        first_half = !first_half;
+    }
+
+    v
+}
+
+fn nibbles3_to_u16(insn_nibbles: Vec<u8>) -> u16 {
+    u16::from_be_bytes(
+        nibbles_to_bytes([0u8].iter().chain(insn_nibbles[1..].iter()))
+    .try_into().unwrap())
 }
 
 impl CPU {
@@ -40,11 +66,37 @@ impl CPU {
             dt: 0,
             st: 0,
             timedelta_error: 0,
+            prng_val: 0x0badf00d
         }
+    }
+
+    pub fn go(&mut self, addr: u16) {
+        self.regs.pc = addr;
+    }
+
+    pub fn get_pc(&self) -> u16 {
+        self.regs.pc
+    }
+
+    pub fn push_word(&mut self, data: u16) {
+        self.regs.sp -= 2;
+        self.write_word(self.regs.sp, data);
+    }
+
+    pub fn pop_word(&mut self) -> u16 {
+        let data =self.read_word(self.regs.sp);
+        self.regs.sp += 2;
+        data
     }
 
     pub fn write_byte(&mut self, addr: u16, data: u8) {
         self.memory[addr as usize] = data;
+    }
+
+    pub fn write_word(&mut self, addr: u16, data: u16) {
+        let bytes = data.to_be_bytes();
+        self.write_byte(addr, bytes[0]);
+        self.write_byte(addr + 1, bytes[1]);
     }
 
     pub fn read_byte(&self, addr: u16) -> u8 {
@@ -53,7 +105,7 @@ impl CPU {
 
     pub fn read_word(&self, addr: u16) -> u16 {
         let bytes : [u8; 2] = self.memory[addr as usize..addr as usize+2].try_into().expect("unable to read at address");
-        u16::from_le_bytes(bytes)
+        u16::from_be_bytes(bytes)
     }
 
     pub fn write_bytes(&mut self, addr: u16, data: &Vec<u8>) {
@@ -71,12 +123,13 @@ impl CPU {
     }
 
     fn do_ret(&mut self) {
-
+        self.regs.pc = self.pop_word();
     }
 
     fn do_call(&mut self, addr: u16) {
         // TODO: push to stack
-        self.regs.pc = addr
+        self.push_word(self.regs.pc + 2);
+        self.regs.pc = addr;
     }
 
     fn clear_screen(&mut self) {
@@ -121,6 +174,39 @@ impl CPU {
         }
     }
 
+    fn skip_cond(&mut self, cond: bool) {
+        if cond {
+            self.regs.pc += 2;
+        }
+    }
+
+    fn random(&mut self) -> u8 {
+        // pg.4, https://doi.org/10.18637%2Fjss.v008.i14
+        // we just ignore the high 3 bytes as we only need a u8
+        self.prng_val ^= self.prng_val << 13;
+        self.prng_val ^= self.prng_val >> 17;
+        self.prng_val ^= self.prng_val << 5;
+        self.prng_val as u8
+    }
+
+    // store `nr` (must be <= 8) registers from `addr`
+    fn store_regs(&mut self, addr: u16, nr: u8) {
+        assert!(nr <= 8);
+
+        for i in 0..nr {
+            self.write_byte(addr + i as u16, self.regs.v_regs[i as usize]);
+        }
+    }
+
+    // load `nr` (must be <= 8) registers from `addr`
+    fn load_regs(&mut self, addr: u16, nr: u8) {
+        assert!(nr <= 8);
+
+        for i in 0..nr {
+            self.regs.v_regs[i as usize] = self.read_byte(addr + i as u16);
+        }
+    }
+
     pub fn clock(&mut self, ms_delta: u32) {
         // decrement timers
         let timer_ticks = (ms_delta + self.timedelta_error) / 16;
@@ -133,6 +219,8 @@ impl CPU {
         let insn_nibbles = bytes_to_nibbles(insn_bytes.iter());
         self.regs.pc += 2;
 
+        println!("executing: {:04x} (bytes: {:02x?}, nibbles: {:x?})", insn, insn_bytes, insn_nibbles);
+
         match insn {
             0x00e0 => self.clear_screen(),
             0x00ee => self.do_ret(),
@@ -140,17 +228,50 @@ impl CPU {
             0x1000..=0x1fff => self.regs.pc = insn & 0xfff,
             // CALL (subroutine call)
             0x2000..=0x2fff => self.do_call(insn & 0xfff),
-            // SE (skip if equal)
-            0x3000..=0x3fff => if self.regs.v_regs[insn_nibbles[1] as usize] == insn_bytes[1] { self.regs.pc += 2 },
-            // SNE (skip if not equal)
-            0x4000..=0x4fff => if self.regs.v_regs[insn_nibbles[1] as usize] != insn_bytes[1] { self.regs.pc += 2 },
-            0x5000..=0x5fff => if self.regs.v_regs[insn_nibbles[1] as usize] == self.regs.v_regs[insn_nibbles[2] as usize] { self.regs.pc += 2 },
+            // SE (skip if equal Vx immediate)
+            0x3000..=0x3fff => self.skip_cond(self.regs.v_regs[insn_nibbles[1] as usize] == insn_bytes[1]),
+            // SNE (skip if not equal Vx immediate)
+            0x4000..=0x4fff => self.skip_cond(self.regs.v_regs[insn_nibbles[1] as usize] != insn_bytes[1]),
+            // SE (skip if equal Vx Vy)
+            0x5000..=0x5fff => self.skip_cond(self.regs.v_regs[insn_nibbles[1] as usize] == self.regs.v_regs[insn_nibbles[2] as usize]),
             0x6000..=0x6fff => self.regs.v_regs[insn_nibbles[1] as usize] = insn_bytes[1],
             0x7000..=0x7fff => self.regs.v_regs[insn_nibbles[1] as usize] = self.regs.v_regs[insn_nibbles[1] as usize] + insn_bytes[1],
             0x8000..=0x8fff => self.binary_reg_op(insn_nibbles[1], insn_nibbles[2], insn_nibbles[3]),           
-            
+            // register SNE (skip if not equal Vx Vy)
+            0x9000..=0x9fff => self.skip_cond(self.regs.v_regs[insn_nibbles[1] as usize] != self.regs.v_regs[insn_nibbles[2] as usize]),
+            // set I to immediate value
+            0xa000..=0xafff => self.regs.i = nibbles3_to_u16(insn_nibbles),
+            // jump to immediate offset by V0
+            0xb000..=0xbfff => self.regs.pc = nibbles3_to_u16(insn_nibbles) + self.regs.v_regs[0] as u16,
+            // generate a random byte and store its AND with immediate value in Vx
+            0xc000..=0xcfff => self.regs.v_regs[insn_nibbles[1] as usize] = self.random() & insn_bytes[1],
+            // Draw N-byte sprite
+            // 0xd000..=0xdfff => ,
+            // Skip if key Vx is pressed
+            // 0xe09e..=0xef9e => ,
+            // Skip if key Vx is not pressed
+            // 0xe0a1..=0xefa1 => ,
+            // Load Vx with the delay timer value
+            0xf007..=0xff07 => self.regs.v_regs[insn_nibbles[1] as usize] = self.dt,
+            // Wait for a keypress and store the key in Vx
+            // 0xf00a..=0xff0a => ,
+            // Set delay timer value to Vx
+            0xf015..=0xff15 => self.dt = self.regs.v_regs[insn_nibbles[1] as usize],
+            // Set sound timer to Vx
+            0xf018..=0xff18 => self.st = self.regs.v_regs[insn_nibbles[1] as usize],
+            // set I = I + vx
+            0xf01e..=0xff1e => self.regs.i = self.regs.v_regs[insn_nibbles[1] as usize] as u16 + self.regs.i,
+            // set I to the location of sprite for digit Vx
+            // 0xf029..=0xff29 => ,
+            // store the BCD representation of Vx in I..I+2
+            // 0xf033..=0xff33 => 
+            // store V0 through Vx from I..I+x
+            0xf055..=0xff55 => self.store_regs(self.regs.i, insn_nibbles[1]),
+            // load V0 through Vx from I..I+x
+            0xf065..=0xff65 => self.load_regs(self.regs.i, insn_nibbles[1]),
+
             // Undefined opcode
-            _ => (),
+            _ => panic!("undefined opcode"),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
 pub mod cpu;
 use pretty_hex::*;
 
-fn main() {
-    let mut cpu = cpu::CPU::new();
-    let font = include_bytes!("rom.bin");
-    cpu.write_bytes(0, &font.to_vec());
+use crate::cpu::nibbles_to_bytes;
 
-    println!("{}", pretty_hex(&cpu.read_memory()));
+fn main() {
+    // let mut cpu = cpu::CPU::new();
+    // let font = include_bytes!("rom.bin");
+    // cpu.write_bytes(0, &font.to_vec());
+
+    let mut cpu = cpu::CPU::new();
+    cpu.go(0x200);
+
+    let instructions: Vec<u8> = [
+        0xa1, 0x23,     // set i to 0x123
+    ].to_vec();
+    cpu.write_bytes(0x200, &instructions);
+
+    cpu.clock(1);
+
+    println!("PC ended on {}", cpu.get_pc());
+    // println!("CPU: {:#x?}", cpu.regs);
+    // println!("{}", pretty_hex(&cpu.read_memory()));
 }
